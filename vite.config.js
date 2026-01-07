afff533e43d26376e12730409625853041130397
kahip.chan <kahip.chan@qhms.com>
2026-01-08 01:45:39 +0800

    update: update homepage

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'repo-name' with your actual GitHub repository name (e.g., 'api-portal')
 export default defineConfig({
   plugins: [react()],
-  base: '/repo-name/', 
+  base: '/api-test-portal/', 
 })
